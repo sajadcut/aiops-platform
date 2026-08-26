7497a6bbf1e5249d4e9a31f85bc0baa49b72c58a
sajadcut <sajadcut@gmail.com>
2026-08-26 09:50:31 +0330

    feat(prometheus): support upstream MCP auth header

--- integrations/prometheus/mcp_client.py
+++ integrations/prometheus/mcp_client.py
@@ -20,6 +20,7 @@ class PrometheusMCPClient(MCPClient):
             protocol_version=settings.MCP_PROTOCOL_VERSION,
             timeout=settings.MCP_TIMEOUT_SECONDS,
             bearer_token=settings.MCP_BEARER_TOKEN,
+            authorization_header=settings.PROMETHEUS_MCP_AUTH_HEADER,
             ca_cert_path=settings.MCP_CA_CERT_PATH,
             client_cert_path=settings.MCP_CLIENT_CERT_PATH,
             client_key_path=settings.MCP_CLIENT_KEY_PATH,
