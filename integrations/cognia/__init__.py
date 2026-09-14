06086206efa31dfed911671e82c72a72ce3a7164
sajadcut <sajadcut@gmail.com>
2026-09-14 17:03:41 +0330

    feat: add Cognia RAG integration boundary

---
+++ integrations/cognia/__init__.py
@@ -0,0 +1,13 @@
+from integrations.cognia.client import (
+    CogniaAPIError,
+    CogniaClient,
+    CogniaConfigurationError,
+    CogniaContractError,
+)
+
+__all__ = [
+    "CogniaAPIError",
+    "CogniaClient",
+    "CogniaConfigurationError",
+    "CogniaContractError",
+]
